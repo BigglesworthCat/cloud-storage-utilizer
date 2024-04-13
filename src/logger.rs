60f9012bef4351bdf581cdc7fecafc34bebf5009
Dmytro Kriuchkov <kriuchkovdima@gmail.com>
2024-04-13 15:08:25 +0300

    Added env vars support for logger

--- src/logger.rs
+++ src/logger.rs
@@ -1,12 +1,18 @@
+use std::path::Path;
 use tracing::log::Level;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{EnvFilter, Registry};
 
 pub fn setup_logger() {
+    let log_dir = std::env::var("LOG_DIR").unwrap_or("./logs".to_string());
+    let log_dir = Path::new(log_dir.as_str());
+    let log_file = std::env::var("LOG_FILE").unwrap_or("log.txt".to_string());
+    let log_file = Path::new(log_file.as_str());
+
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(Level::Info.as_str()));
 
-    let file_appender = tracing_appender::rolling::never("./logs", "log.txt");
+    let file_appender = tracing_appender::rolling::never(log_dir, log_file);
     let file_layer = tracing_subscriber::fmt::layer().with_writer(file_appender);
 
     let subscriber = Registry::default().with(env_filter).with(file_layer);
